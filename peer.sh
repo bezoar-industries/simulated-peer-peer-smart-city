2c17ac08a0417693ab20cd7e43d5fe87e0b61027
Matt Muller <justmatt@colostate.edu>
2019-11-11 17:08:16 -0600

    comment

--- peer.sh
+++ peer.sh
@@ -7,7 +7,7 @@ PORT=60000
 REGISTRY=192.168.50.204
 REGISTRY=10.5.28.6
 
-#LOGFILE=logs/csu$NICKNAME.log
+#LOGFILE=logs/csu.log
 LOGFILE=
 
 #java -Xdebug -Xrunjdwp:transport=dt_socket,address=12346,server=y,suspend=y -Djava.util.logging.config.class=cs555.chiba.service.LogConfig -Dcsu.log.file=$LOGFILE cs555.chiba.node.Peer $REGISTRY $PORT 0 0
