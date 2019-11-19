026893d7ae5cf5782c0a10351a7a06745f50a21b
Matt Muller <justmatt@colostate.edu>
2019-11-18 18:41:33 -0600

    Remote start script

---
+++ startupNoShell.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+
+CURRENT_WORKING_DIR=~/CS555-final
+
+START_CLUSTER="cd $CURRENT_WORKING_DIR; nohup nice -n 19 ./peer.sh 2>&1 &"
+
+for cluster in $(cat workers.txt)
+do
+  echo 'logging into '$cluster
+  COMMAND="ssh $cluster \"$START_CLUSTER\""
+  echo $COMMAND
+  eval $COMMAND &
+done
+
