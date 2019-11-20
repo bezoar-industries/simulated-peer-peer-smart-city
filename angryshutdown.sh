13a366dba8140e8c4d8702d02760a4b8975b7d4a
Kevin Bruhwiler <kevincb@funkwerks.cs.colostate.edu>
2019-11-19 18:26:10 -0700

    added file and adjusted scripts for running on lattice cluster

--- angryshutdown.sh
+++ angryshutdown.sh
@@ -5,9 +5,14 @@
 # Use the regular shutdown.sh first.  It uses regular kill that allows our
 # shutdown hook to clean up our sockets and threads.
 
+if [ "$#" -ne 1 ]; then
+echo "Usage:sh startupNoShell.sh <machine_list>"
+exit
+fi
+
 KILL_PROCESSES="PID=\\\$(jps | awk '/Peer|Registry/{print \\\$1}'); kill -9 \\\${PID}"
 
-for cluster in $(cat workers.txt)
+for cluster in $(cat $1)
 do
   echo 'logging into '$cluster
   COMMAND="ssh $cluster \"$KILL_PROCESSES\""
--- startupNoShell.sh
+++ startupNoShell.sh
@@ -1,11 +1,15 @@
 #!/bin/env bash
 
+if [ "$#" -ne 1 ]; then
+echo "Usage:sh startupNoShell.sh <machine_list>"
+exit
+fi
 
 CURRENT_WORKING_DIR=~/CS555-final
 
 START_CLUSTER="cd $CURRENT_WORKING_DIR; nohup nice -n 19 ./lotsofpeers.sh 2>&1 &"
 
-for cluster in $(cat workers.txt)
+for cluster in $(cat $1)
 do
   echo 'logging into '$cluster
   COMMAND="ssh $cluster \"$START_CLUSTER\""
